d6edf2cbd30199ed721a0a8ec89208cf90d13c88
haylie <haylie@palteer.cm>
2024-11-19 01:59:57 -0500

    lastlast

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -44,6 +44,7 @@ const CartItem = ({ onContinueShopping }) => {
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <h3 sytle={{color: 'black'}}>Total Cart Count : {totalItems}</h3>
       <div>
         {cart.map(item => (
           <div className="cart-item" key={item.name}>
